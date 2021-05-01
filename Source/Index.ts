500775b285bbec21f3dd4bab7d262e39cd0069ec
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-05-01 18:23:06 +0530

    Added Geometric Series :eyes:

--- Source/Index.ts
+++ Source/Index.ts
@@ -1,5 +1,20 @@
 class NumberyStuff {
+	static GeometricSeries = {
+		InfiniteSum(a: number, r: number) {
+			if(r > 1) throw new Error("The series is diverging since ratio is greater than 1. It can't be calculated as it tends to infinity");
 
+			if(r === 1) throw new Error("The series is tending to infinity since ratio is equal to 1, meaning the Sum with keep increasing everytime");
+
+			const req = 1 - r;
+
+			return (a / req);
+		},
+		NthTerm(a: number, r: number, n: number) {
+			if(n < 1) throw new Error("n can't be less than 1!");
+
+			return a * (Math.pow(r, (Math.round(n) - 1)));
+		},
+	};
 };
 
 export default NumberyStuff;
---
+++ Tests/GeometricSeries.ts
@@ -0,0 +1,10 @@
+import NumberyStuff from "../Source/Index";
+
+const a = 2;
+const r = 0.5;
+
+console.log(NumberyStuff.GeometricSeries.InfiniteSum(a, r));
+
+const n = 4;
+
+console.log(NumberyStuff.GeometricSeries.NthTerm(a, r, n));
